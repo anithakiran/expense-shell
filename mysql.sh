066480d761aa764dd8321f0c4247033bc2860c4b
anithakiran <anitha1995d@gmail.com>
2024-06-23 14:05:29 +0530

    functions

--- mysql.sh
+++ mysql.sh
@@ -1,49 +1,49 @@
 #!/bin/bash
 
 USERID=$(id -u)
-
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1 )
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
-echo  "please enter DB password"
+echo "Please enter DB password:"
 read -s mysql_root_password
 
-if [ $USERID -ne 0 ]
-then 
-    echo "please run this script with root access"
-    exit 1
-else
-    echo "your are super user"
-fi
-
-echo "script started executing at : $TIMESTAMP"
-
-VALIDATE() {
-if [ $1 -ne 0 ]
-then    
-    echo -e "$2 is $R failure $N "
-else
-    echo -e "$2 is $G successfull $N"
-fi
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
 }
 
-dnf install mysql-server -y  &>> $LOGFILE
-VALIDATE $? "Installing Mysql-Server"
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
 
-systemctl enable mysqld  &>> $LOGFILE
-VALIDATE $? "Enabling mysql-server"
 
-systemctl start mysqld  &>> $LOGFILE
-VALIDATE $? "Starting mysql-server"
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL Server"
 
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabling MySQL Server"
 
-mysql -h devopsaws78s.online -uroot -p${mysql_root_password} -e 'show database'; &>> $LOGFILE
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "Starting MySQL Server"
 
+# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+# VALIDATE $? "Setting up root password"
+
+#Below code will be useful for idempotent nature
+mysql -h devopsaws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
