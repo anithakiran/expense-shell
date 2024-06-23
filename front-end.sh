d954b2b8985f382050c2dca09d4b61c7e67255d1
anithakiran <anitha1995d@gmail.com>
2024-06-23 17:26:46 +0530

    functions

--- front-end.sh
+++ front-end.sh
@@ -39,13 +39,16 @@ VALIDATE $? "Enabling nginx"
 systemctl start nginx &>> $LOGFILE
 VALIDATE $? "Starting nginx"
 
+rm -rf /usr/share/nginx/html/* &>> $LOGFILE
+VALIDATE $? "removing existing content"
+
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
 VALIDATE $? "Downloading frontend code"
 
 cd /usr/share/nginx/html &>>$LOGFILE
 unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "Extracting frontend code"
-
+                  
 cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
 VALIDATE $? "Copied expense conf"
 
